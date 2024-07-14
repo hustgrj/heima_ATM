a52f7052805c4e15b95dd9b814004b03335f39a0
GuoRenjie <guorenjie_cug@163.com>
2024-07-14 23:54:09 +0800

    一定要取消机器学习辅助的代码补齐，否则会一直弹出各种补全内容！

---
+++ src/com/heima/ATM.java
@@ -0,0 +1,12 @@
+package com.heima;
+
+import java.util.ArrayList;
+
+public class ATM {
+    private ArrayList <Account> accounts=new ArrayList<Account>();
+    public void start(){
+
+    }
+
+
+}
